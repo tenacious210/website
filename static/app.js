f1273537573a8d7f8ce767ff73be9098c879065a
tenacious210 <tenacious210@gmail.com>
2022-03-15 08:39:30 -0400

    Fix darkmode not working on homepage

--- static/app.js
+++ static/app.js
@@ -1,10 +1,12 @@
 var button = document.getElementById("usersearchbtn");
 
-button.onclick = function () {
-    var text = document.getElementById("usersearch").value;
-    console.log(window.location.pathname + "?user=" + text);
-    window.location = window.location.pathname + "?user=" + text;
-    return false;
+if(button) {
+    button.onclick = function () {
+        var text = document.getElementById("usersearch").value;
+        console.log(window.location.pathname + "?user=" + text);
+        window.location = window.location.pathname + "?user=" + text;
+        return false;
+    }
 }
 
 document.getElementById('light-dark-mode-toggle').addEventListener('click', () => localStorage.setItem('theme', document.body.classList.toggle('dark-mode') ? 'dark' : 'light'));
