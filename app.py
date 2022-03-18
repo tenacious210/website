02a0ceb758128c44e8af164f77f7a3221b37a8dd
tenacious210 <tenacious210@gmail.com>
2022-03-18 09:12:01 -0400

    Schedule module replaced

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 from emote_grabber import update_emote_stats
+from datetime import date, timedelta
 from flask import Flask
 from views import views
 from time import sleep
 import threading
-import schedule
 import os
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
-schedule.every().day.at("12:00").do(update_emote_stats)
-
 
 def update_stats():
     while True:
-        schedule.run_pending()
-        sleep(50)
+        try:
+            with open("lastupdate.txt", "r") as lutxt:
+                lutxt_content = lutxt.read()
+        except FileNotFoundError:
+            with open("lastupdate.txt", "w") as lutxt:
+                lutxt_content = ""
+        date_str = lutxt_content if lutxt_content.count("-") == 2 else "2020-01-01"
+        year, month, day = [int(n) for n in date_str.split("-")]
+        lastupdate = date(year, month, day)
+        today = date.today()
+        if today - lastupdate != timedelta(0):
+            update_emote_stats()
+            with open("lastupdate.txt", "w") as lutxt:
+                lutxt.write(str(today))
+        sleep(600)
 
 
 if __name__ == "__main__":
-    update_stats_thread = threading.Thread(target=update_stats, daemon=True)
+    update_stats_thread = threading.Thread(target=update_stats)
     update_stats_thread.start()
     server_port = os.environ.get("PORT", "8080")
     app.run(debug=False, port=server_port, host="0.0.0.0")
