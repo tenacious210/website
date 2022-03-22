a1100a3b1f6c5cbc892558d23f6d35e294a50a6e
tenacious210 <tenacious210@gmail.com>
2022-03-22 12:12:39 +0000

    Downloads database from cloud storage

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from google.cloud import storage
 from flask import Flask
 from views import views
 import os
@@ -7,5 +8,9 @@ app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == "__main__":
+    storage_client = storage.Client()
+    storage_bucket = storage_client.bucket("tenadev")
+    blob = storage_bucket.blob("user_emotes.db")
+    blob.download_to_filename("user_emotes.db")
     server_port = os.environ.get("PORT", "8080")
     app.run(debug=False, port=server_port, host="0.0.0.0")
