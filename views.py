78035d394a4a71ba375991fff56a05d61e04dba1
tenacious210 <tenacious210@gmail.com>
2022-09-08 09:36:34 -0400

    Add donation page

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 from emotes.html import *
 from users.html import *
+from dominate.tags import iframe
 
 views = Blueprint(__name__, "views")
 
@@ -41,3 +42,27 @@ def api():
         return emote_top_api(emote)
     else:
         return top5s_api()
+
+
+@views.route("/donate")
+def donate():
+    dono_message = (
+        "This site costs about $10/month to host. If you enjoy my work and "
+        "are feeling generous, a donation of any amount would be much appreciated! "
+    )
+    with div(align="center") as container:
+        emote_to_html("dggL")
+        p(dono_message, align="center", style="width:400px")
+        iframe(
+            id="kofiframe",
+            src="https://ko-fi.com/tenadev/?hidefeed=true&widget=true&embed=true&preview=true",
+            style="border:none;width:400px;height:615px;padding:4px;background:#1e1e1e;",
+            title="tenadev",
+        )
+        p()
+    return render_template(
+        "with_user_search.html",
+        title="donate",
+        header=f"Support me",
+        content=container,
+    )
